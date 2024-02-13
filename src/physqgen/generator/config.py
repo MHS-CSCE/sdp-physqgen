2420db030304ca77dc04716cbec053c660a0e244
AlfarelSynthesis <104072649+AlfarelSynthesis@users.noreply.github.com>
2024-02-13 09:28:04 -0500

    implement config reading, basic config file, add initial config docs

--- src/physqgen/generator/config.py
+++ src/physqgen/generator/config.py
@@ -6,8 +6,8 @@ def parseQuestions(configTextIO: StringIO) -> list:
     """Parses a question config text stream for questions and returns a list of all contained question subclass objects."""
     questions: list = []
 
-    dicts = loadJSON(configTextIO)
-    for question in dicts:
+    dict = loadJSON(configTextIO)
+    for question in dict["questions"]:
         # uses getattr to construct the corresponding class described in the config
         # should change to use some kind of class ID instead of the name directly at some point, to prevent in-code class name changes from breaking old config files
         if "correctRange" in question:
