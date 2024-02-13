19b54b083dd87acc3b565c9e21bb0d233f930aeb
AlfarelSynthesis <104072649+AlfarelSynthesis@users.noreply.github.com>
2024-02-13 08:23:39 -0500

    Implement question config parsing

--- src/physqgen/generator/config.py
+++ src/physqgen/generator/config.py
@@ -2,7 +2,7 @@ from io import StringIO
 from json import load as loadJSON
 from physqgen import generator
 
-def parseQuestions(self, configTextIO: StringIO) -> list:
+def parseQuestions(configTextIO: StringIO) -> list:
     """Parses a question config text stream for questions and returns a list of all contained question subclass objects."""
     questions: list = []
 
@@ -10,6 +10,9 @@ def parseQuestions(self, configTextIO: StringIO) -> list:
     for question in dicts:
         # uses getattr to construct the corresponding class described in the config
         # should change to use some kind of class ID instead of the name directly at some point, to prevent in-code class name changes from breaking old config files
-        questions.append(getattr(generator, question["question"])(question["data"]))
+        if "correctRange" in question:
+            questions.append(getattr(generator, question["question"])(variableConfig=question["variableConfig"], solveVariable=question["solveVariable"], text=question["text"], correctRange=question["correctRange"]))
+        else:
+            questions.append(getattr(generator, question["question"])(variableConfig=question["variableConfig"], solveVariable=question["solveVariable"], text=question["text"]))
 
     return questions
