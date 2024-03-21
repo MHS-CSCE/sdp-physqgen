39425cf77940bb21dd3127afe51cd624a4967aa0
Isabelle <inich3@ocdsb.ca>
2024-03-21 13:55:45 -0400

    Altered the flask files again

---
+++ src/physqgen/app/auth.py
@@ -0,0 +1,28 @@
+"""
+"""
+# importing Flask and other modules
+from flask import Flask, request, render_template, Blueprint
+from os import path
+
+#defining views for routes
+auth = Blueprint('auth', __name__)
+
+
+@auth.route('/')
+def log_in():
+    return render_template('loginpage.html')
+
+
+   
+
+""" if form is submitted by user
+    if request.method == "POST":
+        getting input from said form
+        FIRST_NAME= request.form.get("name") 
+        LAST_NAME = request.form.get("last-name") 
+        EMAIL_A = request.form.get("email-address")
+
+        if EMAIL_A = ms jones email 
+            mb send to the teacher view? 
+
+    return render_template("loginpage.html")"""
