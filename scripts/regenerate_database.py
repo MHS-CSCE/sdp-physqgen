1a1c1397404e00301837b1eb7cc5fdaa680d90e4
AlfarelSynthesis <104072649+AlfarelSynthesis@users.noreply.github.com>
2024-05-08 09:12:08 -0400

    Add final script header

--- scripts/regenerate_database.py
+++ scripts/regenerate_database.py
@@ -1,4 +1,9 @@
+"""
+Last Modified: May 8, 2024
+"""
+
 from physqgen.admin.create_database import createDataBaseFromBlank
 
 if __name__ == "__main__":
+    # this can also be done using the admin app's clear data button
     createDataBaseFromBlank()
