40f255edf943ebb0b71783948c71de81dd9bb26a
AlfarelSynthesis <104072649+AlfarelSynthesis@users.noreply.github.com>
2024-05-09 21:15:26 -0400

    clean up view.py code, move things around and clarify some logic

--- src/physqgen/app/constants.py
+++ src/physqgen/app/constants.py
@@ -3,3 +3,6 @@ from os.path import join
 # the path to the sqlite3 database
 # relative to main.py
 DATABASEPATH = join(".", "data", "data.db")
+
+# relative to view.py
+IMG_FOLDER_PATH = join('.', 'static', 'images')
--- src/physqgen/app/view.py
+++ src/physqgen/app/view.py
@@ -3,7 +3,7 @@ from os.path import join
 from flask import (Blueprint, redirect, render_template, request, session,
                    url_for, Response)
 
-from physqgen.app.constants import DATABASEPATH
+from physqgen.app.constants import DATABASEPATH, IMG_FOLDER_PATH
 from physqgen.session import Session
 
 views = Blueprint('views', __name__)
@@ -16,9 +16,6 @@ def redirectpage() -> str:
     """
     return render_template("index.html")
 
-
-#for image display purposes
-IMG_FOLDER_PATH = join('static', 'images')
 @views.route('/qpage', methods=['GET', 'POST'])
 def qpage() -> str | Response:
     """
@@ -30,6 +27,10 @@ def qpage() -> str | Response:
         # also extracts session data for ease of use
         sessionData: dict = session["session"]
     except KeyError:
+        return redirect(url_for("auth.log_in"), code=302)
+    # happens when a session that no longer exists tries to access page, as the data fetched from the database will be None and not behave correctly
+    # this should only happen if the database is cleared mid-session, but may as well redirect instead of showing error page
+    except TypeError:
         return redirect(url_for("auth.log_in"), code=302)
 
     if request.method == "POST":
@@ -52,50 +53,54 @@ def qpage() -> str | Response:
             # increment to next active question and reload
             if ((sessionObject.active_question + 1) < len(sessionObject.questions)) and activeQuestion.correct:
                 sessionObject.incrementActiveQuestionData()
-            # correct & last question
-            elif activeQuestion.correct:
-                sessionObject.updateSessionDataInDatabase()
-                return redirect(url_for("views.exit"), code=302)
             else:
-                # incorrect answer
+                # update separately if it isn't handled by the increment
                 sessionObject.updateSessionDataInDatabase()
+                
+                # correct & last question
+                if activeQuestion.correct:
+                    return redirect(url_for("views.exit"), code=302)
+        
+            # pass data back, so any change are visible on page
+            # doesn't need to happen if invalid answer was submitted
+            session["session"] = sessionObject
         
         except ValueError:
             # don't count as a submission if the input is not a float value
             pass
-
-        # pass data back, so page updates and etc.
-        session["session"] = sessionObject
-
-        return redirect(url_for("views.qpage"), code=302)
-
-    # GET request
-
-    # redirect to exit page if have already completed all questions
-    if len(sessionData["questions"]) == sessionData["questions_correct"]:
-        return redirect(url_for("views.exit"), code=302)
+    else:
+        # GET request
+        # redirect to exit page if have already completed all questions
+        if len(sessionData["questions"]) == sessionData["questions_correct"]:
+            return redirect(url_for("views.exit"), code=302)
 
     # fetch image filename for active question
     file = join(IMG_FOLDER_PATH, sessionData["questions"][sessionData["active_question"]]["imageName"])
     
     return render_template("questionpage.html", image=file)
 
-@views.route('/exit', methods = ['GET', 'POST'])
-def exit() -> str:
+@views.route('/exit', methods = ['GET'])
+def exit() -> str | Response:
     """
     Last page of the generator, used as a transition page to tell the user to exit.\n
-    Returns an HTML template.
+    Returns an HTML template or a Response.
     """
+    # if there is no registered session, redirect to login page
     try:
         session["session"]
     except KeyError:
         return redirect(url_for("auth.log_in"), code=302)
+    # happens when a session that no longer exists tries to access page, as the data fetched from the database will be None and not behave correctly
+    # this should only happen if the database is cleared mid-session, but may as well redirect instead of showing error page
+    except TypeError:
+        return redirect(url_for("auth.log_in"), code=302)
     
     # load session, updates the counter for number of questions correct
-    session["session"] = Session.recreateSession(DATABASEPATH, session["session"])
+    sessionObject = Session.recreateSession(DATABASEPATH, session["session"])
+    session["session"] = sessionObject
     
-    # check if have gotten all questions correct, redirect to question page if not so
-    if session["session"].questions_correct != len(session["session"].questions):
+    # check if have gotten all questions correct, redirect to question page if not
+    if sessionObject.questions_correct != len(sessionObject.questions):
         return redirect(url_for("views.qpage"), code=302)
 
     return render_template("exit.html")
