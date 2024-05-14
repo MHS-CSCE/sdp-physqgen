a9404aa4aae54969fba1899fe95a4bf159d9ba9a
AlfarelSynthesis <104072649+AlfarelSynthesis@users.noreply.github.com>
2024-05-14 19:42:46 -0400

    Improve verification checks and docs

--- src/physqgen/generator/config/variable.py
+++ src/physqgen/generator/config/variable.py
@@ -18,22 +18,26 @@ class VariableConfig:
     
     def nonZero(self) -> bool:
         """Checks if the configured range is allowed, disallowing it from including 0.0. Returns True if valid, False if not valid."""
-        # if the bounds are different signs, they will be below 0.0
-        # if both bounds are 0.0, they will equal 0.0
-        # if one bound is 0.0, the other checks will catch them assuming the first doesn't
         return not any(
             (
+                # this check applies if the range crosses 0, or includes 0.0 or 0 as one endpoint
                 float(self.range[0] * self.range[1]) <= 0.0,
+                # these checks are extra, to ensure that floating point calculations don't make the above not exactly 0.0 even if one bound is
+                # afaik, these should never apply if the above doesn't, but it doesn't hurt much to include them given the uncertainty around floating point number equality
                 float(self.range[0]) == 0.0,
-                float(self.range[1]) == 0.0
+                float(self.range[1]) == 0.0,
+                # these would likely only result from mistyping, but may still mess up the checks above
+                float(self.range[0]) == -0.0,
+                float(self.range[1]) == -0.0
             )
         )
     
     def nonOverlapping(self, other) -> bool:
-        """Returns True if the ranges of this VariableConfig and other (also a VariableConfig) do not overlap, False otherwise. Used to check if two VariableConfig ranges are valid in terms of each-other"""
+        """Returns True if the ranges of this VariableConfig and other (also a VariableConfig) do not overlap, False otherwise."""
         # all other cases are invalid elsewhere
         r1low = self.range[0]
         r1high = self.range[1]
         r2low = other.range[0]
         r2high = other.range[1]
-        return (r1low > r2high or r1high < r2low)
+        # check if they are overlapping and reverse it, it's a simpler check
+        return not (r1low <= r2high and r1high >= r2low)
